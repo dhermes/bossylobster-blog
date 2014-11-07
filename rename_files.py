efc85be37cabad360b8732a9a8912413aec86cf9
Danny Hermes <daniel.j.hermes@gmail.com>
2014-11-07 11:38:39 -0800

    Renaming files to match blogger URLs.

--- rename_files.py
+++ rename_files.py
@@ -2,6 +2,7 @@
 # file rename.
 
 import os
+import glob
 import json
 
 # In order to pull down all (36) of my blog posts I ran:
@@ -17,18 +18,28 @@ VALID_MONTHS = ['%02d' % val for val in xrange(1, 12 + 1)]
 
 with open('blog_relative_urls.json', 'r') as fh:
     RELATIVE_URLS = json.load(fh)
+# This is a relative path, this code assumes it is running
+# from the root of this git repository.
+EXISTING_POSTS = set(glob.glob('content/*md'))
 
 
+cleaned_titles = {}
 for blog_url in RELATIVE_URLS:
     year, month, title_as_html = blog_url.split('/')
     if year not in VALID_YEARS or month not in VALID_MONTHS:
         raise ValueError(blog_url)
-    # This is a relative path, meant to be run from root of
-    # this git repository.
     name, ext = os.path.splitext(title_as_html)
     if ext != '.html':
         raise ValueError(blog_url)
     title_as_md = name + '.md'
-    relative_path = os.path.join('content', title_as_md)
-    if not os.path.exists(relative_path):
-        raise ValueError(blog_url)
+    key = (year, month, title_as_md)
+    cleaned_titles[key] = os.path.join('content', title_as_md)
+
+
+if set(cleaned_titles.values()) != EXISTING_POSTS:
+    cleaned_only = cleaned_titles - EXISTING_POSTS
+    existing_only = EXISTING_POSTS - cleaned_titles
+    raise ValueError(('Filenames do not match blog titles',
+                      cleaned_only, existing_only))
+else:
+    print 'Filenames match blog titles.'
