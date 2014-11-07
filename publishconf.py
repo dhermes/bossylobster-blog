79a37761c6d21d4af1b01ee875618e5d252a3cf1
Danny Hermes <daniel.j.hermes@gmail.com>
2014-11-07 10:28:21 -0800

    Customizing publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+# This file is used by the `publish` command in the Makefile.
 
 import os
 import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.bossylobster.com'
-RELATIVE_URLS = False
-
+SITEURL = 'https://dhermes.github.io'
+DELETE_OUTPUT_DIRECTORY = True
+RELATIVE_URLS = True
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-
-DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = None
+# GOOGLE_ANALYTICS = None
